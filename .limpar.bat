895cbffe442a8c911056950dfffa15327078ac64
laosioor <alingofilho@gmail.com>
2025-12-17 12:55:02 -0300

    first commit

---
+++ .limpar.bat
@@ -0,0 +1,44 @@
+@echo off
+
+rem Limpeza.
+rem Desenvolvido por David Buzatto
+
+del *.aux
+del *.bak
+del *.bbl
+del *.bcf
+del *.blg
+del *.brf
+del *.def
+del *.dvi
+del *.equ
+del *.exp
+del *.gz
+del *.gz(busy)
+del *.idx
+del *.ilg
+del *.lis
+del *.listing
+del *.lof
+del *.log
+del *.loq
+del *.lot
+del *.minted
+del *.nav
+del *.out
+del *.pdf
+del *.ps
+del *.pyg
+del *.sav
+del *.sigla
+del *.siglax
+del *.snm
+del *.stf
+del *.symbols
+del *.symbolsx
+del *.synctex
+del *.synctex(busy)
+del *.toc
+del *.xml
+del *.xwm
+del *-blx.bib
